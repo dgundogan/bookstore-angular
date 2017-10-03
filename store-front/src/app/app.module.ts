62dc328f0a9c66a946b822dbac22354ee63a29a9
Dursun <dgundogan@gmail.com>
2017-10-03 21:55:57 +0300

    store-front 1

--- store-front/src/app/app.module.ts
+++ store-front/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HttpModule } from '@angular/http';
+import 'hammerjs';
+import { HomeComponent } from './components/home/home.component';
+import { routing } from './app.routing';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
